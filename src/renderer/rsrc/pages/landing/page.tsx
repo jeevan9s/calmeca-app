7d2d0e1bce058e02ee1689eedc4450300eacd846
jeevansanchez <166963895+jeevansanchez@users.noreply.github.com>
2025-05-22 00:29:37 -0400

    night

--- src/renderer/rsrc/pages/landing/page.tsx
+++ src/renderer/rsrc/pages/landing/page.tsx
@@ -1,10 +1,10 @@
 import Titlebar from "../../components/Titlebar"
+import '@/renderer/rsrc/styles/landing.css'
 
 export default function Landing() {
-    return (
-        <div id="landing-page">
-            <Titlebar />
-
-        </div>
-    )
+  return (
+    <div id="landing-page">
+      <Titlebar />
+    </div>
+  )
 }
