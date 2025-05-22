6b0b8f33e1acc80574195bd5663d4cd03e7f7d7a
Jeevan Sanchez <jeevansanchez42@gmail.com>
2025-05-22 15:13:31 -0400

    sidebar-toggle, little bug

--- src/renderer/rsrc/components/Titlebar.tsx
+++ src/renderer/rsrc/components/Titlebar.tsx
@@ -1,10 +1,15 @@
 import { Minus, X, Square, Menu } from 'react-feather'
 import { PiCopy } from 'react-icons/pi'
+import { ChevronsRight } from 'react-feather'
 import '@/renderer/rsrc/styles/tb.css'
 import { useState, useEffect } from 'react'
 
 export default function TitleBar() {
   const [isMaximized, setisMaximized] = useState(false)
+  const [sidebarHovered, setSidebarHovered ] = useState(false)
+  const [sidebarVisible, setSidebarVisible] = useState(false)
+
+
 
   useEffect(() => {
     const onMax = () => setisMaximized(true)
@@ -19,19 +24,41 @@ export default function TitleBar() {
     }
   }, [])
 
+  const handleSidebarHover = () => {
+    setSidebarHovered(true)
+    setSidebarVisible(true)
+  }
+
+  const handleSidebarLeave = () => {
+    setSidebarHovered(false)
+    setSidebarVisible(false)
+  }
+
+
+
+
+
   return (
     <div id="titlebar" className="drag relative w-full h-10 flex items-center">
-      <div id="left-bar" className="absolute left-0 flex gap-10 no-drag mt-2 mb-1 ml-2">
-        <button id="sidebar">
-          <Menu color="white" size={16} strokeWidth={2} className='ml-10' />
-        </button>
-      </div>
+<div
+  id="left-bar"
+  className="absolute left-0 flex no-drag mt-2 mb-1 ml-5"
+  onMouseEnter={handleSidebarHover}
+  onMouseLeave={handleSidebarLeave}
+>
+  <button id="sidebar">
+    {sidebarHovered ? (
+      <ChevronsRight className='sidebar-tog' color="white" size={18} strokeWidth={2} />
+    ) : (
+      <Menu id="menu" color="white" size={16} strokeWidth={2} />
+    )}
+  </button>
+</div>
 
-      <div id="right-bar" className="absolute right-0 flex gap-10 no-drag mr-8 mt-2 mb-1">
+      <div id="right-bar" className="absolute right-0 flex no-drag">
         <button id="minimize" onClick={() => window.ipcRenderer.send('minimize')}>
           <Minus color="white" size={16} strokeWidth={2} />
         </button>
-
         <button
           id="maximize"
           onClick={() => window.ipcRenderer.send(isMaximized ? 'restore' : 'maximize')}
@@ -42,7 +69,6 @@ export default function TitleBar() {
             <Square color="white" size={13} strokeWidth={2} />
           )}
         </button>
-
         <button id="close" onClick={() => window.ipcRenderer.send('close')}>
           <X color="white" size={16} strokeWidth={2} />
         </button>
