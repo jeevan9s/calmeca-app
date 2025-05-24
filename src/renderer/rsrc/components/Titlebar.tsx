7a692375ad02b74715d7696cee48b6e5da1a7898
Jeevan Sanchez <jeevansanchez42@gmail.com>
2025-05-24 18:45:32 -0400

    calendar-icon

--- src/renderer/rsrc/components/Titlebar.tsx
+++ src/renderer/rsrc/components/Titlebar.tsx
@@ -1,15 +1,14 @@
-import { Minus, X, Square, Menu } from 'react-feather'
 import { PiCopy } from 'react-icons/pi'
-import { ChevronsRight } from 'react-feather'
+import { ChevronsRight, ChevronsUp, Minus, X, Square, Menu, Calendar } from 'react-feather'
 import '@/renderer/rsrc/styles/tb.css'
 import { useState, useEffect } from 'react'
 
 export default function TitleBar() {
   const [isMaximized, setisMaximized] = useState(false)
-  const [sidebarHovered, setSidebarHovered ] = useState(false)
+  const [sidebarHovered, setSidebarHovered] = useState(false)
   const [sidebarVisible, setSidebarVisible] = useState(false)
-
-
+  const [calendarHovered, setCalendarHovered] = useState(false)
+  const [calendarVisible, setCalendarVisible] = useState(false)
 
   useEffect(() => {
     const onMax = () => setisMaximized(true)
@@ -34,47 +33,69 @@ export default function TitleBar() {
     setSidebarVisible(false)
   }
 
+  const handleCalendarHover = () => {
+    setCalendarHovered(true)
+    setCalendarVisible(true)
+  }
 
-
-
+  const handleCalendarLeave = () => {
+    setCalendarHovered(false)
+    setCalendarVisible(false)
+  }
 
   return (
     <div id="titlebar" className="drag relative w-full h-9 flex items-center">
-<div
-  id="left-bar"
-  className="absolute left-0 flex no-drag mt-2 mb-1 ml-5"
-  onMouseEnter={handleSidebarHover}
-  onMouseLeave={handleSidebarLeave}
->
-  <div id = "subleft-bar" className='flex items-center gap-5'>
-{/*  */}
+      <div id="left-bar" className="absolute left-0 flex no-drag mt-2 mb-1 ml-5">
+        <div
+          id="subleft-bar"
+          className="flex items-center gap-5"
+          onMouseEnter={handleSidebarHover}
+          onMouseLeave={handleSidebarLeave}
+        >
+          <button id="logo">
+            <img src="/taskbar.png" alt="Logo" className="h-4 w-4 p-0" />
+          </button>
 
-<button id = "logo">
-   <img
-  src="/taskbar.png"
-  alt="Logo"
-  className="h-4 w-4 p-0"
-/>
-</button>
+          <button id="sidebar" className="relative w-6 h-6">
+            <Menu
+              className={`sidebar-icon ${sidebarHovered ? 'icon-hidden' : 'icon-visible'}`}
+              color="white"
+              size={14}
+              strokeWidth={2}
+            />
+            <ChevronsRight
+              className={`sidebar-icon ${sidebarHovered ? 'icon-visible' : 'icon-hidden'}`}
+              color="white"
+              size={20}
+              strokeWidth={2}
+            />
+          </button>
+          </div>
 
-
-<button id="sidebar" className="relative w-6 h-6">
-  <Menu
-    className={`sidebar-icon ${sidebarHovered ? 'icon-hidden' : 'icon-visible'}`}
-    color="white"
-    size={14}
-    strokeWidth={2}
-  />
-  <ChevronsRight
-    className={`sidebar-icon ${sidebarHovered ? 'icon-visible' : 'icon-hidden'}`}
-    color="white"
-    size={20}
-    strokeWidth={2}
-  />
-</button>
-</div>
-
-</div>
+          <div
+            id="calendar-cntr"
+            className="flex items-center gap-5 ml-40"
+            onMouseEnter={handleCalendarHover}
+            onMouseLeave={handleCalendarLeave}
+          >
+            <button id="calendar" className="relative w-6 h-6">
+              <Calendar
+                id = "cd-icon"
+                className={`calendar-icon ${calendarHovered ? 'icon-hidden' : 'icon-visible'}`}
+                color="white"
+                size={20}
+                strokeWidth={2}
+              />
+              <ChevronsUp
+                id = "arrow-up"
+                className={`calendar-icon ${calendarHovered ? 'icon-visible' : 'icon-hidden'}`}
+                color="white"
+                size={20}
+                strokeWidth={2}
+              />
+            </button>
+          </div>
+      </div>
 
       <div id="right-bar" className="absolute right-0 flex no-drag">
         <button id="minimize" onClick={() => window.ipcRenderer.send('minimize')}>
