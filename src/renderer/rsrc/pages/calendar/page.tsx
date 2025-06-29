e2b6d842d58a05d90387cba9d939a928fca51ac8
jeevansanchez <166963895+jeevansanchez@users.noreply.github.com>
2025-06-29 14:10:46 -0400

    init some main pages

---
+++ src/renderer/rsrc/pages/calendar/page.tsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+export default function Calendar() {
+    console.log("Calendr page loaded")
+
+    return (
+        <div id = "calendar-page" className="min-h-screen bg-black/30">
+            <Layout />
+        </div>
+    )
+}
---
+++ src/services/courseService.ts
@@ -0,0 +1,4 @@
+// Course Service File
+import { db } from "./db";
+import { Course } from "./db";
+import { v4 as uuid} from 'uuid'
