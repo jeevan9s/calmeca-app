6bed7488c89cd1f2490c8987d9203cb590f1c33c
jeevansanchez <166963895+jeevansanchez@users.noreply.github.com>
2025-06-30 02:59:12 -0400

    note servicing

---
+++ src/services/noteService.ts
@@ -0,0 +1,52 @@
+// Calendar Service File
+import { db, Note, Course } from "./db";
+import { v4 as uuid} from 'uuid'
+
+// impl crud, fetching notes by course, most recent 
+
+export const addNote = async (note: Omit<Note, 'id' | 'createdOn' | 'updatedOn' | 'color'>): Promise<Note> => {
+    const course: Course | undefined = await db.courses.get(note.courseId)
+    if (!course) throw new Error("Course not found")
+    
+        const now = new Date()
+        const newNote: Note = {
+            ...note,
+            id: uuid(),
+            createdOn: now,
+            updatedOn: now, // later on implement
+            color: course.color
+        }
+        await db.notes.add(newNote)
+        return newNote
+}
+
+export const deleteNote = async (id: string) => {
+    return db.notes.delete(id)
+}
+
+export const updateNote = async (id:string, updates: Partial<Omit<Note, 'id' | 'createdOn'>>): Promise<void> => {
+    updates.updatedOn = new Date()
+    await db.notes.update(id, updates)
+}
+
+export const getAllNotes = async (): Promise<Note[]> => {
+    return db.notes.toArray()
+}
+
+export const getNotesByCourse = async(courseId: string): Promise<Note[]> => {
+    return db.notes.where('courseId').equals(courseId).toArray()
+}
+
+export const getNoteById = async(id: string): Promise<Note | undefined> => {
+    return db.notes.get(id)
+}
+
+export const getNotesByTag = async (tag: string): Promise<Note[]> => {
+    const allNotes = await db.notes.toArray();
+    return allNotes.filter(note => note.tags?.includes(tag));
+};
+
+export const getMostRecentNotes = async (limit = 10): Promise<Note[]> => {
+    return db.notes.orderBy('updatedOn').reverse().limit(limit).toArray()
+    
+}
